18f9cd1c331d303831a26790a27badd983c979ba
PetrAsDigital <pbecak@opentable.com>
2017-04-17 22:00:22 +1000

    data load from an api implemented

--- App.jsx
+++ App.jsx
@@ -4,26 +4,38 @@ var FakeData = require('./FakeData.json');
 
 
 class App extends React.Component {
-   constructor() {
-      super();
+    constructor() {
+        super();
 
-      this.state = FakeData;
-   }
-	
-   render() {
-      return (
-         <div>
-            <Header/>
-            <div className='maingrid'>
-                <table>
-                <tbody>
-                    {this.state.data.map((obj, i) => <TableRow key = {i} data = {obj} />)}
-                </tbody>
-                </table>
+        this.state = { data: [] };
+    }
+
+    componentDidMount() {
+        //this.setState({ data: FakeData.data }); 
+
+        fetch('http://localhost:8081/get')
+        .then(response => {
+            return response.json();
+        }).then(body => {
+            this.setState({ data: body.data })
+        });
+    };
+
+    render() {
+        //console.log(this.state.data);
+        return (
+            <div>
+                <Header/>
+                <div className='maingrid'>
+                    <table>
+                    <tbody>
+                        {this.state.data.map((obj, i) => <TableRow key = {i} data = {obj} />)}
+                    </tbody>
+                    </table>
+                </div>
             </div>
-         </div>
-      );
-   }
+        );
+    }    
 }
 
 class Header extends React.Component {
